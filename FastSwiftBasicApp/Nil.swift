a9a1b7be7c17977d0476734fcacb4ca3d085d9c5
bs0439 <kbs0439@gmail.com>
2023-10-17 23:22:23 +0900

    study optional, swich

---
+++ FastSwiftBasicApp/Nil.swift
@@ -0,0 +1,18 @@
+//
+//  Nil.swift
+//  FastSwiftBasicApp
+//
+//  Created by 김범수 on 10/17/23.
+//
+
+import SwiftUI
+
+struct Nil: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Nil()
+}
---
+++ FastSwiftBasicApp/Selection.swift
@@ -0,0 +1,18 @@
+//
+//  Selection.swift
+//  FastSwiftBasicApp
+//
+//  Created by 김범수 on 10/17/23.
+//
+
+import SwiftUI
+
+struct Selection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Selection()
+}
