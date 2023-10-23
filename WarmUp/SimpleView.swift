9dff5ea280c23ce634a844cd06ba4f2b83894456
bs0439 <kbs0439@gmail.com>
2023-10-23 22:40:01 +0900

    study ui

---
+++ WarmUp/SimpleView.swift
@@ -0,0 +1,46 @@
+//
+//  SimpleView.swift
+//  WarmUp
+//
+//  Created by 김범수 on 10/23/23.
+//
+
+import SwiftUI
+
+struct SimpleView: View {
+    var body: some View {
+        VStack{
+            Image(systemName: "pencil")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+            
+            Text("헤드라인 입니다.")
+                .font(.headline)
+                .bold()
+                .padding()
+            
+            Text("서브헤드라인 입니다.")
+                .font(.subheadline)
+                .padding()
+            
+            Text("바디 입니다.")
+                .font(.body)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Click me")
+                    .padding()
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .bold()
+            })
+        }
+    }
+}
+
+#Preview {
+    SimpleView()
+}
